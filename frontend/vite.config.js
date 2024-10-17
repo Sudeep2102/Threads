906bcb8e30c36b62d293d45a42fabc1d53f136f1
Sudeep Sahu <sudeepsahu58@gmail.com>
2024-10-17 11:22:55 +0530

    Testing commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,16 @@
+import { defineConfig } from 'vite'; // Ensure this line is present
+import react from '@vitejs/plugin-react'; // React plugin
+
 export default defineConfig({
 	plugins: [react()],
 	server: {
 	  port: 3000,
 	  proxy: {
 		"/api": {
-		  target: "http://backend:5000",
+		  target: "http://localhost:5000",
 		  changeOrigin: true,
 		  secure: false,
 		},
 	  },
 	},
-  });
-  
+});
