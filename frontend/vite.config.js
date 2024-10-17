3e0dc22fc336639d74a14dc6e7c67566891408e6
Sudeep Sahu <sudeepsahu58@gmail.com>
2024-10-17 10:41:55 +0530

    Testing commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	server: {
-		port: 3000,
-		// Get rid of the CORS error
-		proxy: {
-			"/api": {
-				target: "http://backend:5000",
-				changeOrigin: true,
-				secure: false,
-			},
+	  port: 3000,
+	  proxy: {
+		"/api": {
+		  target: "http://backend:5000",
+		  changeOrigin: true,
+		  secure: false,
 		},
+	  },
 	},
-});
+  });
+  
